d39f6ccea2dbefc66defe74ff1a18c04664d318f
万宜成 <wanyichen>
2018-12-27 11:20:44 +0800

    test

--- pages/watching-detail/watching-detail.js
+++ pages/watching-detail/watching-detail.js
@@ -18,100 +18,195 @@ Page({
     ec_2: {
       lazyLoad: true
     },
-    vrData: {
-        "Id":"9452730d-f590-43ab-9905-73a9d98ed46f",
-        "Note":"视频监测图片1",
-        "PhotoUrl":"http://images.pccoo.cn/bbs/20111110/20111110231732125.jpg",
-        "DateTime":"2018-12-26T08:05:15.2771488+08:00",
-        "MonitorId":"b1390edb-ea4c-422c-a9fb-fc554b40f812",
-        "PointMonitorInfos":[
-            {
-                "PointId":1,
-                "PointType":23,
-                "PointName":"水位",
-                "CurrentValue":"48mm",
-                "Unit":"mm",
-                "Data":[
-                    {
-                        "data":[
-                            [
-                                145456456456,
-                                56.99
-                            ],
-                            [
-                                145456456488,
-                                56.99
-                            ]
-                        ],
-                        "name":"水位",
-                        "datatype":null
-                    }
-                ]
-            },
-            {
-                "PointId":2,
-                "PointType":24,
-                "PointName":"降雨量",
-                "CurrentValue":"20mm",
-                "Unit":"mm",
-                "Data":[
-                    {
-                        "data":[
-                            [
-                                145456456456,
-                                56.99
-                            ],
-                            [
-                                145456456488,
-                                56.99
-                            ]
-                        ],
-                        "name":"降雨量",
-                        "datatype":null
-                    }
-                ]
-            },
-            {
-                "PointId":2,
-                "PointType":28,
-                "PointName":"渗流",
-                "CurrentValue":"20pa",
-                "Unit":"pa",
-                "Data":[
-                    {
-                        "data":[
-                            [
-                                145456456456,
-                                56.99
-                            ],
-                            [
-                                145456456488,
-                                56.99
-                            ]
-                        ],
-                        "name":"渗流",
-                        "datatype":null
-                    }
-                ]
-            }
-        ]
-    }
+    vrData: {}
   },
   onLoad: function (e) {
     this.getData(e.id)
-    const _this = this
-    app.globalData.setTitle(_this.data.vrData.Note)
   },
   getData: function (photoid) {
     const _this = this
     const id = app.globalData.defaultMonitor.Id
-    const url = `reach/mobile/monitorobjects/videophotodetail/${id}/photoid/${photoid}`
+    const url = `reach/mobile/videophotodetail/${id}/photoid/${photoid}`
     app.globalData.fetch({
       url: url,
+      closeLoading: true,
       cb: (res) => {
         console.log(res)
-        
+        _this.setData({
+          vrData: res.data.Result
+        })
+        _this.timeFormat()
+        app.globalData.setTitle(_this.data.vrData.Note)
+        _this.data.vrData.PointMonitorInfos.map((item, index)=>{
+          _this.updateChart({
+            item: item,
+            index: index
+          })
+        })
       }
     })
+  },
+  timeFormat: function() {
+    this.data.vrData.DateTime = this.data.vrData.DateTime.split('.')[0].replace("T", " ")
+    this.setData({
+      vrData: this.data.vrData
+    })
+  },
+  updateChart: function(params) {
+    const _this = this
+    const { type = 'init', item, index } = params
+
+    if (!item.Data[0].data.length) {
+      wx.showToast({
+        title: item.Data.name + '图表暂无数据',
+        icon: 'none',
+        duration: 1500
+      })
+      return false
+    }
+    const dataList = item.Data[0].data
+    const chart_x = []
+    const chart_y = []
+    let chart_x_interval = 0
+    let ThresholdValue = 1000000
+    dataList.map((dataItem, dataIndex) => {
+      const time = new Date(dataItem[0])
+      chart_x.push(util.formatTime(time, 'hh:mm'))
+      chart_y.push(dataItem[1])
+      chart_x_interval = Math.floor(dataList.length / 7)
+    })
+
+    const chart_title = `${item.PointName}数据分析(${item.Unit})`
+    const option = {
+      title: {
+        text: chart_title,
+        left: 'left',
+        top: 10,
+        left: 10,
+        textStyle: {
+          color: "#fff",
+          fontSize: 12
+        }
+      },
+      itemStyle: {
+        color: ''
+      },
+      grid: {
+        left: 40,
+        right: 40,
+        bottom: 30,
+        top: 40,
+        containLabel: false
+      },
+      xAxis: {
+        type: 'category',
+        boundaryGap: false,
+        data: chart_x,
+        axisLine: {
+          lineStyle: {
+            color: "#CBCBCB"
+          },
+        },
+        axisLabel: {
+          align: 'center',
+          interval: chart_x_interval,
+          color: '#fff',
+          fontSize: 10
+        }
+        // show: false
+      },
+      yAxis: [
+        {
+          x: 'center',
+          type: 'value',
+          min: function(value) {
+            return (value.min - value.min/10)
+          },
+          max: function(value) {
+            return (value.max + value.max/10)
+          },
+          axisLine: {
+            lineStyle: {
+              color: "#CBCBCB"
+            }
+          },
+          splitLine: {
+            lineStyle: {
+              type: 'dashed'
+            },
+            show: false
+          },
+          minInterval: 1,
+          axisLabel: {
+            color: '#fff',
+            fontSize: 10
+          }
+        }
+      ],
+      series: [{
+        name: chart_title,
+        type: 'line',
+        symbol:'none',
+        smooth: true,
+        markLine: {
+          symbol: ['none', 'none'],
+          label: {
+            show: false
+          },
+          lineStyle: {
+              color: '#FF9900',
+              type: 'solid'
+          },  
+          data: [
+            {
+              type: 'max',
+              name: '阈值',
+              yAxis: ThresholdValue
+            },
+          ]
+        },
+        areaStyle: {
+          color: '#0076FF'
+        },
+        lineStyle: {
+          normal: {
+            width: 0.5,
+            color:'#fff'  
+          }
+        },
+        data: chart_y
+      }]
+    }
+
+    if (item.PointType === 24) {
+      option.series[0].type = 'bar'
+      //option.series[0].barWidth = '10%'
+      option.xAxis.boundaryGap = true
+      option.itemStyle.color = "#0076FF"
+    }
+
+    if (item.PointType === 28) {
+      delete option.series[0].areaStyle
+    }
+
+    let chart = index === 0 ? chart_0 : index === 1 ? chart_1 : chart_2
+    const element = "#watchingchar_" + index
+    
+    if (type === 'update') {
+      chart && chart.setOption(option)
+      return false
+    }
+
+    _this[`echartsComponnet${index}`] = _this.selectComponent(element)
+    _this[`echartsComponnet${index}`].init((canvas, width, height) => {
+      chart = echarts.init(canvas, null, {
+        width: width,
+        height: height
+      })
+      canvas.setChart(chart)
+      chart.setOption(option)
+      return chart
+    })
+
   }
 })
--- pages/watching-list/watching-list.js
+++ pages/watching-list/watching-list.js
@@ -4,204 +4,25 @@ const app = getApp()
 
 Page({
   data: {
-    warningInfo: null,
-    warningList: null,
     allPage: 1,
     timeActive: 0,
     timeList: ['1小时', '一天', '一周'],
-    asycDownNums: 0,
-    asycMaxNums: 2,
     activeType: 0, // 0, 1, 2, 3
     activeDate: 0,
     activeTimeStart: '2018-11-8',
     activeTimeEnd: '2018-11-8',
     nowPage: 1,
-    vrData:[
-      {
-            "Id":"9ad04843-4b17-460f-a244-9bd61ded55a0",//照片Id
-            "Note":"视频监测图片1",
-            "PhotoUrl":"http://www.chinairn.com/UserFiles/image/20170727/20170727094140_4541.jpeg",
-            "DateTime":"2018-12-26T08:02:48.5534954+08:00",
-            "MonitorId":"d4a168a1-c485-4a1a-af6b-87d0aa9bcfd5",//监测物id
-            "PointMonitorInfos":[
-                {
-                    "PointId":1,
-                    "PointType":23,
-                    "PointName":"水位",
-                    "CurrentValue":"48mm",
-                    "Unit":null,
-                    "Data":null
-                },
-                {
-                    "PointId":2,
-                    "PointType":24,
-                    "PointName":"降雨量",
-                    "CurrentValue":"20mm",
-                    "Unit":null,
-                    "Data":null
-                },
-                {
-                    "PointId":2,
-                    "PointType":28,
-                    "PointName":"渗流",
-                    "CurrentValue":"20pa",
-                    "Unit":null,
-                    "Data":null
-                }
-            ]
-        },
-        {
-            "Id":"78a364e4-2c6b-474d-bfd0-48ac46f90b4f",
-            "Note":"视频监测图片2",
-            "PhotoUrl":"http://images.pccoo.cn/bbs/20111110/20111110231732125.jpg",
-            "DateTime":"2018-12-26T09:02:48.5544924+08:00",
-            "MonitorId":"5af362e3-5c88-4eab-9035-449342bd3ea5",
-            "PointMonitorInfos":[
-                {
-                    "PointId":1,
-                    "PointType":23,
-                    "PointName":"水位",
-                    "CurrentValue":"48mm",
-                    "Unit":null,
-                    "Data":null
-                },
-                {
-                    "PointId":2,
-                    "PointType":24,
-                    "PointName":"降雨量",
-                    "CurrentValue":"20mm",
-                    "Unit":null,
-                    "Data":null
-                },
-                {
-                    "PointId":2,
-                    "PointType":28,
-                    "PointName":"渗流",
-                    "CurrentValue":"20pa",
-                    "Unit":null,
-                    "Data":null
-                }
-            ]
-        },
-        {
-            "Id":"77f299fe-f784-4218-a848-7fb7359591b8",
-            "Note":"视频监测图片3",
-            "PhotoUrl":"https://img.pconline.com.cn/images/upload/upc/tx/photoblog/1206/18/c11/12051116_12051116_1340030473921_mthumb.jpg",
-            "DateTime":"2018-12-26T10:02:48.5544924+08:00",
-            "MonitorId":"6a8e57ca-c077-43f3-b545-c9aa7eea1779",
-            "PointMonitorInfos":[
-                {
-                    "PointId":1,
-                    "PointType":23,
-                    "PointName":"水位",
-                    "CurrentValue":"48mm",
-                    "Unit":null,
-                    "Data":null
-                },
-                {
-                    "PointId":2,
-                    "PointType":24,
-                    "PointName":"降雨量",
-                    "CurrentValue":"20mm",
-                    "Unit":null,
-                    "Data":null
-                },
-                {
-                    "PointId":2,
-                    "PointType":28,
-                    "PointName":"渗流",
-                    "CurrentValue":"20pa",
-                    "Unit":null,
-                    "Data":null
-                }
-            ]
-        },
-        {
-            "Id":"3bb4bc80-ab3e-41ed-ba9c-a32539151acd",
-            "Note":"视频监测图片4",
-            "PhotoUrl":"http://photocdn.sohu.com/20120630/Img346934936.jpg",
-            "DateTime":"2018-12-26T11:02:48.5544924+08:00",
-            "MonitorId":"862e23c5-e943-4322-af25-7d65c73a2045",
-            "PointMonitorInfos":[
-                {
-                    "PointId":1,
-                    "PointType":23,
-                    "PointName":"水位",
-                    "CurrentValue":"48mm",
-                    "Unit":null,
-                    "Data":null
-                },
-                {
-                    "PointId":2,
-                    "PointType":24,
-                    "PointName":"降雨量",
-                    "CurrentValue":"20mm",
-                    "Unit":null,
-                    "Data":null
-                },
-                {
-                    "PointId":2,
-                    "PointType":28,
-                    "PointName":"渗流",
-                    "CurrentValue":"20pa",
-                    "Unit":null,
-                    "Data":null
-                }
-            ]
-        }
-    ]
+    vrData:[]
   },
   onLoad: function () {
-    this.resetTime()
-    this.getWarningInfo()
-    this.getWarningList({})
+    
   },
   onShow: function () {
-    this.timeFormat(this.data.vrData)
-    if (app.globalData.refreshPage) {
-      this.getWarningInfo()
-      this.getWarningList({})
-      app.globalData.refreshPage = false
-    }
-  },
-  resetTime: function () {
-    let date = util.formatTime(new Date(), 'yyyy-MM-dd')
-    this.setData({
-      timeActive: 0,
-      activeTimeStart: date,
-      activeTimeEnd: date
-    })
-  },
-  closeLoading: function () {
-    if (this.data.asycDownNums === this.data.asycMaxNums) {
-      this.setData({
-        asycDownNums: 0
-      })
-      wx.hideLoading()
-    }
-  },
-  getWarningInfo: function () {
-    const id = app.globalData.defaultMonitor.Id
-    const _this = this
-    app.globalData.fetch({
-      url: `reach/mobile/getwarningstaticinfo/${id}`,
-      cb: (res) => {
-        console.log(res)
-        if (res.data.Result) {
-           _this.setData({
-            warningInfo: res.data.Result
-          })
-          _this.setData({
-            asycDownNums: ++_this.data.asycDownNums
-          })
-          _this.closeLoading()
-        }
-      }
-    })
+    this.getWarningList({})
   },
   getWarningList: function (params) {
     const _this = this
-    const { type = 'init', nowPage = 1, closeLoading = false } = params
+    const { type = 'init', nowPage = 1, closeLoading = true } = params
 
     if (nowPage > _this.data.allPage) {
       return false
@@ -211,37 +32,32 @@ Page({
     })
 
     const id = app.globalData.defaultMonitor.Id
-    //const warningtype = _this.data.activeType
     const starttime = _this.data.activeTimeStart
     const endtime = _this.data.activeTimeEnd
     const page = _this.data.nowPage
     const showType = _this.data.timeActive
       
     app.globalData.fetch({
-      url: `reach/mobile/monitorobjects/videophotolist/${id}/showtype/${showType}/starttime/${starttime}/endtime/${endtime}/nowPage/${page}`,
+      url: `reach/mobile/videophotolist/${id}/showtype/${showType}/starttime/${starttime}/endtime/${endtime}/nowPage/${page}`,
+      closeLoading: closeLoading,
       cb: (res) => {
         console.log(res)
-        // if (res.data.Result) {
-        //    const array = type === 'init' ?  res.data.Result.DataList : _this.data.warningList.concat(res.data.Result.DataList)
-        //    _this.setData({
-        //     warningList: array,
-        //     allPage: res.data.Result.AllPage
-        //   })
-        //   _this.setData({
-        //     asycDownNums: ++_this.data.asycDownNums
-        //   })
-        //   _this.closeLoading()
-        //   if (nowPage > 1 || closeLoading) {
-        //     wx.hideLoading()
-        //   }
-        //   if (!_this.data.warningList.length) {
-        //     wx.showToast({
-        //       title: '当前选择区间暂无数据',
-        //       icon: 'none',
-        //       duration: 1500
-        //     })
-        //   }
-        // }
+        
+        if (res.data.Result && res.data.Result.DataList.length) {
+           const array = type === 'init' ?  res.data.Result.DataList : _this.data.vrData.concat(res.data.Result.DataList)
+           _this.setData({
+            vrData: array,
+            allPage: res.data.Result.AllPage
+          })
+        }
+        if (!res.data.Result.DataList.length) {
+            wx.showToast({
+              title: '当前选择区间暂无数据',
+              icon: 'none',
+              duration: 1500
+            })
+          }
+        _this.timeFormat(_this.data.vrData)
       }
     })
   },
@@ -321,7 +137,6 @@ Page({
     list.map((item, index)=>{
       item.DateTime = item.DateTime.split('.')[0].replace("T", " ")
     })
-    console.log(list)
     this.setData({
       vrData: list
     })
